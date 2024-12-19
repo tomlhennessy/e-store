ecfa5f7e03969906b7a89275179fb9e80daa8027
Tom Hennessy <tomlhennessy@outlook.com>
2024-12-19 10:23:24 +1000

    layout wrapper

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout(props) {
+    const {children} = props
+
+  return (
+    <div>
+        <Header />
+
+        <main>
+            {children}
+        </main>
+        <Footer />
+    </div>
+  )
+}
