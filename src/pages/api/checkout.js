3b9568da6ee3da22cf0ecd5a1e768c9245d35399
Tom Hennessy <tomlhennessy@outlook.com>
2024-12-30 15:25:53 +1000

    update success/cancel urls and env variables

--- src/pages/api/checkout.js
+++ src/pages/api/checkout.js
@@ -26,8 +26,8 @@ export default async function handler(req, res) {
     const session = await stripe.checkout.sessions.create({
       line_items: body.lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3000/success',
-      cancel_url: 'http://localhost:3000/cancel'
+      success_url: `${req.headers.origin}/success`,
+      cancel_url: `${req.headers.origin}/cancel`
     });
 
     // Respond with session URL
